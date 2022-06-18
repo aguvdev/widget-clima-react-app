75ce38e78fb178fa16dbe0d212bff38a05bca9ee
hannagustinvega <hannheitiragustin@hotmail.com>
2022-06-18 17:57:02 -0300

    add: componente weatherForm + estilos

--- src/components/weatherForm.js
+++ src/components/weatherForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import styles from "./weatherForm.module.css";
+
 export default function WeatherForm({ onChangeCity }) {
   const [city, setCity] = useState('');
 
@@ -19,6 +21,6 @@ export default function WeatherForm({ onChangeCity }) {
   }
 
   return <form onSubmit={handleSubmit}>
-    <input type='text' onChange={onChange} />
+    <input className={styles.input} type='text' onChange={onChange} />
   </form>;
 }
